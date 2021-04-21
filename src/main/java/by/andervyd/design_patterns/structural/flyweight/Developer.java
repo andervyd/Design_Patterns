f72c9d876c69cb90ccf10a93a64a4c5ace717c39
andervyd <andervyd@gmail.com>
2021-04-21 14:09:20 +0300

    add pattern: Flyweight

---
+++ src/main/java/by/andervyd/design_patterns/structural/flyweight/Developer.java
@@ -0,0 +1,5 @@
+package by.andervyd.design_patterns.structural.flyweight;
+
+public interface Developer {
+    public void writeCode();
+}
---
+++ src/main/java/by/andervyd/design_patterns/structural/flyweight/JavaDeveloper.java
@@ -0,0 +1,8 @@
+package by.andervyd.design_patterns.structural.flyweight;
+
+public class JavaDeveloper implements Developer {
+    @Override
+    public void writeCode() {
+        System.out.println("Java developer writes java code ...");
+    }
+}
